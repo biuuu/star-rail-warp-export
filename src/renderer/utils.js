145c80d31af30ac15dbe2ba37c052b9f7812034d
BlackHazel <BlackHazel@users.noreply.github.com>
2023-09-23 18:48:41 +0200

    fix: Update utils.js for French HSR.
    For some reason mihoyo renamed "cônes de lumière" to "Cône de lumière" and "Personnages" to "Personnage". This patch only includes the newer term without removing the old ones for retro-compatibility.

--- src/renderer/utils.js
+++ src/renderer/utils.js
@@ -1,11 +1,11 @@
 import * as IconComponents from '@element-plus/icons-vue'
 
 const weaponTypeNames = new Set([
-  '光锥', '光錐', 'Lichtkegel', 'Light Cone', 'Conos de luz', 'cônes de lumière', '光円錐', '광추', 'Cones de Luz', 'Световые конусы', 'Nón Ánh Sáng'
+  '光锥', '光錐', 'Lichtkegel', 'Light Cone', 'Conos de luz', 'cônes de lumière', '光円錐', '광추', 'Cones de Luz', 'Световые конусы', 'Nón Ánh Sáng', 'Cône de lumière'
 ])
 
 const characterTypeNames = new Set([
-  '角色', 'Figur', 'Character', 'Personajes', 'Personnages', 'Karakter', 'キャラクター', '캐릭터', 'Personagens', 'Персонажи', 'ตัวละคร', 'Nhân Vật'
+  '角色', 'Figur', 'Character', 'Personajes', 'Personnages', 'Karakter', 'キャラクター', '캐릭터', 'Personagens', 'Персонажи', 'ตัวละคร', 'Nhân Vật', 'Personnage'
 ])
 
 const isCharacter = (name) => characterTypeNames.has(name)
